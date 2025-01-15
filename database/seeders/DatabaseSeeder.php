af5747f78a3ed9b5b010af10f36018efc2c9cd5d
λrmando <ackurtcobain15@gmail.com>
2025-01-14 21:30:47 -0600

    Create seeders for the user
    and set email instead of the name in the states

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,24 +2,18 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    protected array $users = [
-        ['name' => 'Armando Calderón', 'email' => 'armando@gmail.com'],
-        ['name' => 'Guillermo Rodriguez', 'email' => 'guillermo.rodriguez@banpay.com'],
-    ];
-
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        foreach ($this->users as $data) {
-            User::firstOrCreate($data, ['password' => Hash::make('password')])->assignRole('admin');
-        }
+        $this->call([
+            RoleSeeder::class,
+            UserSeeder::class,
+        ]);
     }
 }
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    protected array $users = [
+        ['name' => 'Armando Calderón', 'email' => 'armando@gmail.com'],
+        ['name' => 'Guillermo Rodriguez', 'email' => 'guillermo.rodriguez@banpay.com'],
+    ];
+
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        foreach ($this->users as $data) {
+            User::firstOrCreate($data, ['password' => Hash::make('password')])->assignRole('admin');
+        }
+    }
+}
