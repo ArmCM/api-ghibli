da8acfecea3170b8e7a093f4fdf9e63155324643
λrmando <ackurtcobain15@gmail.com>
2025-01-14 22:31:39 -0600

    Implement endpoint login and validate data

---
+++ app/Http/Controllers/Api/v1/AuthController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\v1\LoginUserRequest;
+use App\Models\User;
+use App\Traits\ApiResponses;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    use ApiResponses;
+
+    public function login(LoginUserRequest $request)
+    {
+        if (!Auth::attempt($request->validated())) {
+            return $this->error('Credentials not match', 401);
+        }
+
+        $user = User::firstWhere('email', $request->validated('email'));
+
+        return $this->ok('Authenticated', [
+            'token' => $user->createToken('api token for'. $user->email)->plainTextToken,
+        ]);
+    }
+}
