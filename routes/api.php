3a673d0418353975dc82dc982589bdb01288e354
λrmando <ackurtcobain15@gmail.com>
2025-01-15 11:35:26 -0600

    Create validation rules for create a user

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\v1\AuthController;
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\v1\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
@@ -14,5 +14,6 @@ Route::middleware('auth:sanctum')->group(function () {
     |--------------------------------------------------------------------------
     */
 
+    Route::post('/users', [UserController::class, 'store']);
 
 });
