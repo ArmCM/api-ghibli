f94b84a32293cf7685785fb2cf8e163f2cefb5c3
λrmando <ackurtcobain15@gmail.com>
2025-01-15 11:35:26 -0600

    Create validation rules for create a user

--- tests/Feature/Api/UserRegistrationTest.php
+++ tests/Feature/Api/UserRegistrationTest.php
@@ -5,17 +5,39 @@ namespace Tests\Feature\Api;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use PHPUnit\Framework\Attributes\Test;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 use Tests\TestCase;
 
 class UserRegistrationTest extends TestCase
 {
     use RefreshDatabase;
 
+    /**
+     * Set up the test.
+     *
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        Permission::create(['name' => 'view.user']);
+        Permission::create(['name' => 'update.user']);
+        Permission::create(['name' => 'delete.user']);
+
+        Role::create(['name' => 'films'])->givePermissionTo([
+            'view.user',
+            'update.user',
+            'delete.user',
+        ]);
+    }
+
     public function validAttributes(array $overrides = []): array
     {
         return array_replace_recursive([
             'name' => 'John Doe',
-            'email' => 'john_doe@example.com',
+            'email' => 'john_doe@gmail.com',
             'password' => 'valid-password',
             'role' => 'admin',
         ], $overrides);
@@ -24,9 +46,9 @@ class UserRegistrationTest extends TestCase
     #[Test]
     public function an_admin_can_create_a_user()
     {
-        User::factory()->admin()->create();
+        $user = User::factory()->admin()->create();
 
-        $response = $this->post('/api/v1/users', $this->validAttributes([
+        $response = $this->actingAs($user)->post('/api/v1/users', $this->validAttributes([
             'role' => 'films',
         ]));
 
@@ -34,10 +56,10 @@ class UserRegistrationTest extends TestCase
 
         $this->assertDatabaseHas('users', [
             'name' => 'John Doe',
-            'email' => 'john_doe@example.com',
+            'email' => 'john_doe@gmail.com',
         ]);
 
-        $user = User::where('email', 'john_doe@example.com')->first();
+        $user = User::where('email', 'john_doe@gmail.com')->first();
         $this->assertTrue($user->hasRole('films'));
     }
 }
