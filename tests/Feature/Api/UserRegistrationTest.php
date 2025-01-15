c3c54c218c0ad04e33b85478dee0be3d42e948a3
λrmando <ackurtcobain15@gmail.com>
2025-01-15 12:06:59 -0600

    Validate fields with invalid formats

--- tests/Feature/Api/UserRegistrationTest.php
+++ tests/Feature/Api/UserRegistrationTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature\Api;
 
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\Test;
 use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
@@ -61,5 +62,42 @@ class UserRegistrationTest extends TestCase
 
         $user = User::where('email', 'john_doe@gmail.com')->first();
         $this->assertTrue($user->hasRole('films'));
+    }
+
+    #[DataProvider('userRegistrationValidationScenarios')]
+    public function test_fields_to_validate_a_user($field, $input, $message)
+    {
+        $user = User::factory()->admin()->create();
+
+        $response = $this->actingAs($user)->post('/api/v1/users', $this->validAttributes([
+            $field => $input
+        ]));
+
+        $response->assertStatus(422);
+
+        $response->assertExactJson([
+            'message' => 'Error de validación.',
+            'errors' => [
+                $field => [
+                    $message,
+                ]
+            ]
+        ]);
+    }
+
+    public static function userRegistrationValidationScenarios(): array
+    {
+        return [
+            ['name', '', 'El campo nombre es obligatorio.'],
+            ['name', '~/$$#', 'El formato del campo nombre es inválido.'],
+            ['name', 'a', 'El campo nombre debe contener entre 2 y 75 caracteres.'],
+            ['email', '', 'El campo correo electrónico debe ser una dirección de correo válida.'],
+            ['email', 'invalid-email', 'El campo correo electrónico debe ser una dirección de correo válida.'],
+            ['email', 'john@invalid-dns.com', 'El campo correo electrónico debe ser una dirección de correo válida.'],
+            ['password', '', 'El campo contraseña es obligatorio.'],
+            ['password', 'asdf', 'El campo contraseña debe contener entre 8 y 20 caracteres.'],
+            ['role', '', 'El campo role es obligatorio.'],
+            ['role', 'invalid-role', 'El campo role es inválido.'],
+        ];
     }
 }
