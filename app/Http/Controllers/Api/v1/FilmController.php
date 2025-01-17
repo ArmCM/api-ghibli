005efda98650e2f5be607c166a6491445e14b456
λrmando <ackurtcobain15@gmail.com>
2025-01-17 02:27:28 -0600

    Can serach with query string in the films endpoint

--- app/Http/Controllers/Api/v1/FilmController.php
+++ app/Http/Controllers/Api/v1/FilmController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\v1;
 
 use App\Http\Controllers\Controller;
 use App\Traits\ApiResponses;
+use App\Utils\QueryString;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Http;
@@ -16,60 +17,14 @@ class FilmController extends Controller
     {
         Gate::authorize('view.all.films');
 
-        $response = Http::get('https://ghibliapi.vercel.app/films', $request->query());
+        $queryParameters = QueryString::toArray($request->getQueryString());
+
+        $response = Http::get('https://ghibliapi.vercel.app/films', $queryParameters);
 
         if ($response->failed()) {
             $this->error('No se pudo conectar al servicio externo.', $response->status());
         }
 
         return $response->json();
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
     }
 }
