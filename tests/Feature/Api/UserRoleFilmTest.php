a7c0034e9c35b5231629e9cf9b6abb7ac5a808de
λrmando <ackurtcobain15@gmail.com>
2025-01-16 22:04:21 -0600

    Can append field in query string for search

--- tests/Feature/Api/UserRoleFilmTest.php
+++ tests/Feature/Api/UserRoleFilmTest.php
@@ -39,4 +39,14 @@ class UserRoleFilmTest extends TestCase
             "code" => 403
         ]);
     }
+
+    #[Test]
+    public function can_append_field_in_films_endpoint_for_search()
+    {
+        $user = User::factory()->films()->create();
+
+        $response = $this->actingAs($user)->get("/api/v1/films?id=2baf70d1-42bb-4437-b551-e5fed5a87abe");
+
+        $response->assertStatus(200);
+    }
 }
